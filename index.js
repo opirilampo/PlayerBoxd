b2eb386d79db0abd5a6f2e3965dba0b3b0095e11
opirilampo <drumfortunakaua23@.com>
2024-05-01 18:54:33 -0300

    jhonny

--- index.js
+++ index.js
@@ -15,3 +15,43 @@ function toggleExpand() {
 }
 
 expandButton.addEventListener('click', toggleExpand);
+
+
+
+const openModalButton = document.getElementById('openModal');
+const closeModalButton = document.getElementById('closeModal');
+const modal = document.getElementById('modal');
+
+openModalButton.addEventListener('click', () => {
+    modal.style.display = 'flex';
+});
+
+closeModalButton.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
+
+
+
+function SignUp() {
+var username = document.getElementById("username").value;
+var email = document.getElementById("email").value;
+var senha = document.getElementById("senha").value;
+var con_senha = document.getElementById("con_senha").value;
+
+
+var conf = 0
+
+if (con_senha != senha) {
+    document.getElementById("con_senha").value = ""
+    conf = 1
+}
+
+
+if (conf == 0) {
+    localStorage.setItem('username', username)
+    localStorage.setItem('email', email)
+    localStorage.setItem('senha', senha)
+    localStorage.setItem('nome', nome)
+    
+}
+}
